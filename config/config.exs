3f1e95c15ae86a82fe62835ba989df93c33bf089
John Mark Untalan <jm@aelogica.com>
2017-11-01 07:37:09 +0800

    Add create_game route with game_field template

---
+++ config/config.exs
@@ -0,0 +1,23 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# Configures the endpoint
+config :gallows, GallowsWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "yq/XKNWPtT5h3Mx5sdNH/yuAOLasMW0FkA2cptZ82wgmC0WUyz1rAiHkDtGX/O0G",
+  render_errors: [view: GallowsWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Gallows.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
---
+++ lib/gallows_web/views/hangman_view.ex
@@ -0,0 +1,9 @@
+defmodule GallowsWeb.HangmanView do
+  use GallowsWeb, :view
+
+  def words_so_far(letters) do
+    letters
+    |> Enum.join(" ")
+  end
+
+end
