25756f620f4ffd4060614b538f42245d02496501
John Mark Untalan <jm@aelogica.com>
2017-11-02 12:44:26 +0800

    Add gallows image

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -3,6 +3,9 @@ defmodule GallowsWeb.HangmanView do
 
   import GallowsWeb.Views.Helpers.GameStateHelper
 
+  def turn(left, target) when target >= left, do: "opacity: 1"
+  def turn(_, _), do: "opacity: 0.1"
+
   def game_over?(state) do
     state in [:won, :lost]
   end
