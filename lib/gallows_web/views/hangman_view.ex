dbd2a0cdd84ed005f7cfc276e7f4cf187bde99d8
John Mark Untalan <jm@aelogica.com>
2017-11-02 09:19:48 +0800

    More Complex Helpers module done

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -1,9 +1,36 @@
 defmodule GallowsWeb.HangmanView do
   use GallowsWeb, :view
 
-  def words_so_far(letters) do
+  def game_over?(state) do
+    state in [:won, :lost]
+  end
+
+  def concat_letters(letters) do
     letters
     |> Enum.join(" ")
+  end
+
+  @responses %{
+    :good_guess   => { "success", "Good guess!" },
+    :bad_guess    => { "warning", "Sorry, bad move" },
+    :already_used => { "info", "You already used that" },
+    :won          => { "success", "You WON!" },
+    :lost         => { "danger",  "You LOST!" },
+  }
+
+  def game_status(state) do
+    @responses[state]
+    |> alert()
+  end
+
+  def alert(nil), do: ""
+  def alert({class, message}) do
+    """
+    <div class="alert alert-#{class}">
+      #{message}
+    </div>
+    """
+    |> raw()
   end
 
 end
